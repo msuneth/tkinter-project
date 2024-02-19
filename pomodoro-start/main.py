793c0ec1f77a4a36fd3708e68a3537d613dfa7eb
msuneth <msuneth@gmail.com>
2024-02-19 17:23:20 +0530

    pomodoro timer project-gui- completed

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -9,7 +10,15 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
+def start_button_clicked():
+    pass
+
+
+def reset_button_clicked():
+    pass
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
@@ -18,12 +27,25 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=100,pady=50,bg=YELLOW)
+window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200,height=224,bg=YELLOW,highlightthickness=0)
+
+timer_label = Label(text="Timer",fg=GREEN,bg=YELLOW, font=(FONT_NAME, 40))
+timer_label.grid(row=0, column=1)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
-canvas.create_image(100,112,image=tomato_img)
-canvas.create_text(100,130,text="00:00",fill="white",font=(FONT_NAME,35,"bold"))
-canvas.pack()
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(row=1, column=1)
+
+start_button = Button(text="Start", command=start_button_clicked)
+start_button.grid(row=2, column=0)
+
+reset_button = Button(text="Reset", command=reset_button_clicked)
+reset_button.grid(row=2, column=3)
+
+checkmark_label = Label(text="✔",fg=GREEN,bg=YELLOW, font=(FONT_NAME, 20))
+checkmark_label.grid(row=3, column=1)
 
 window.mainloop()
