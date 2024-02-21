a182b0c14a765fe6638a8a58a735f5e87aa467ca
msuneth <msuneth@gmail.com>
2024-02-21 10:00:26 +0530

    password-manager-generate password method

--- password-manager/main.py
+++ password-manager/main.py
@@ -1,12 +1,47 @@
 import time
 from tkinter import *
 from tkinter import messagebox
+import random
 import math
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password_clicked():
-    pass
+    # Password Generator Project
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = []
+
+    password_list = [random.choice(letters) for char in range(nr_letters)]
+    password_list += [random.choice(symbols) for char in range(nr_symbols)]
+    password_list += [random.choice(numbers) for char in range(nr_numbers)]
+    # for char in range(nr_letters):
+    #     password_list.append(random.choice(letters))
+    #
+    # for char in range(nr_symbols):
+    #     password_list += random.choice(symbols)
+    #
+    # for char in range(nr_numbers):
+    #     password_list += random.choice(numbers)
+
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+
+    # for char in password_list:
+    #     password += char
+
+    print(f"Your password is: {password}")
+    password_entry.delete(0, 'end')
+    password_entry.insert(0,password)
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
