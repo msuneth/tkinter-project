a7323e190cb7789163b21312648e9089d8eef3a5
msuneth <msuneth@gmail.com>
2024-02-21 07:32:18 +0530

    password-manager-data saved to file

--- password-manager/main.py
+++ password-manager/main.py
@@ -1,3 +1,4 @@
+import time
 from tkinter import *
 import math
 
@@ -9,7 +10,17 @@ def generate_password_clicked():
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def add_password_clicked():
-    pass
+    website_name = website_entry.get()
+    email_username = email_username_entry.get()
+    password = password_entry.get()
+
+    with open("data.txt", "a") as file:
+        file.writelines(f"{website_name} | {email_username} | {password}\n")
+    status_label.config(text="Password data saved")
+    status_label.config(text="")
+    website_entry.delete(0, 'end')
+    password_entry.delete(0, 'end')
+    status_label.config(text="")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -32,8 +43,10 @@ password_label.grid(row=3, column=0)
 
 website_entry = Entry(width=50)
 website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
 email_username_entry = Entry(width=50)
 email_username_entry.grid(row=2, column=1, columnspan=2)
+email_username_entry.insert(0, "msunethbit@gmail.com")
 password_entry = Entry(width=32)
 password_entry.grid(row=3, column=1)
 
@@ -43,5 +56,7 @@ generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=43, command=add_password_clicked)
 add_button.grid(row=4, column=1, columnspan=2)
 
+status_label = Label(text="")
+status_label.grid(row=5, column=1)
 
 window.mainloop()
